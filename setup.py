3cc2471bf1a2876e64195d98dd7c43b863f57ad2
lomik <r.lomonosov@gmail.com>
2013-10-17 02:13:16 +0400

    working snapshot reader

---
+++ setup.py
@@ -0,0 +1,74 @@
+import os
+
+BUILD_STATIC = True
+
+# for static build with tarantool sources
+TARANTOOL_REV = "3536b7002e5ba88d3cdd0ebfbf668762a89dc72c" # release 1.4.8
+
+try:
+    from setuptools import setup, Extension
+    extra_params = dict(test_suite = 'tests',)
+except ImportError:
+    from distutils.core import setup, Extension
+    extra_params = {}
+
+def sh(command):
+    import subprocess
+    ret = subprocess.call(command,shell=True)
+    if ret != 0:
+        raise ValueError("command failed: %s" % command)
+    return ret
+
+def download_tarantool_src():
+    import subprocess
+
+    src_dir = os.path.join(os.path.dirname(__file__),'tarantool_src')
+    if not os.path.exists(src_dir):
+        sh("git clone https://github.com/tarantool/tarantool.git %s" % src_dir)
+        sh("cd %s && git checkout %s" % (src_dir, TARANTOOL_REV))
+
+    return src_dir
+
+
+sources = ["tarantool_snapshot.c",]
+include_dirs = []
+library_dirs = []
+extra_link_args = []
+
+if BUILD_STATIC:
+    tarantool_src_dir = download_tarantool_src()
+    include_dirs += [
+        tarantool_src_dir,
+        os.path.join(tarantool_src_dir,"include"),
+        os.path.join(tarantool_src_dir,"connector","c","include"),
+    ]
+    sources += [
+        os.path.join(tarantool_src_dir,'connector','c','tntrpl','tnt_snapshot.c'),
+        os.path.join(tarantool_src_dir,'connector','c','tnt','tnt_mem.c'),
+        os.path.join(tarantool_src_dir,'connector','c','tntrpl','tnt_log.c'),
+        os.path.join(tarantool_src_dir,'connector','c','tnt','tnt_tuple.c'),
+        os.path.join(tarantool_src_dir,'connector','c','tnt','tnt_stream.c'),
+        os.path.join(tarantool_src_dir,'connector','c','tnt','tnt_request.c'),
+        os.path.join(tarantool_src_dir,'connector','c','tnt','tnt_iter.c'),
+        os.path.join(tarantool_src_dir,'connector','c','tnt','tnt_enc.c'),
+        os.path.join(tarantool_src_dir,'connector','c','tnt','tnt_reply.c'),
+        os.path.join(tarantool_src_dir,'third_party','crc32.c'),
+    ]
+else:
+    extra_link_args += ["-ltarantoolrpl",]
+
+module1 = Extension('tarantool_snapshot',
+                    include_dirs = include_dirs,
+                    library_dirs = library_dirs,
+                    sources = sources,
+                    extra_link_args = extra_link_args)
+
+setup (name = 'Tarantool snapshot',
+       description = 'Tarantool snapshot reader',
+       version='1.0',
+       author='Lomonosov Roman',
+       author_email='r.lomonosov@gmail.com',
+       url='https://github.com/lomik/python-tarantool_snaphot',
+       packages=[],
+       ext_modules = [module1], **extra_params)
+
---
+++ tarantool_snapshot.c
@@ -0,0 +1,181 @@
+#include <Python.h>
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#include <tarantool/tnt.h>
+#include <tarantool/tnt_net.h>
+#include <tarantool/tnt_snapshot.h>
+
+static PyObject *SnapshotError;
+
+typedef struct {
+    PyObject_HEAD
+	struct tnt_stream stream;
+	struct tnt_iter iter;
+	struct tnt_iter iter_tuple;
+	int open_exception;
+} SnapshotIterator;
+
+static PyMethodDef SnapshotIterator_Methods[] = {
+    {NULL}  /* Sentinel */
+};
+
+static int SnapshotIterator_init(SnapshotIterator *self, PyObject *args);
+PyObject* SnapshotIterator_del(SnapshotIterator *self);
+PyObject* SnapshotIterator_iter(SnapshotIterator *self);
+PyObject* SnapshotIterator_iternext(SnapshotIterator *self);
+
+static PyTypeObject SnapshotIterator_Type = {
+    PyObject_HEAD_INIT(NULL)
+    0,                         /*ob_size*/
+    "tarantool_snapshot.SnapshotIterator",      /*tp_name*/
+    sizeof(SnapshotIterator),      /*tp_basicsize*/
+    0,                         /*tp_itemsize*/
+    (destructor)SnapshotIterator_del,             /*tp_dealloc*/
+    0,                         /*tp_print*/
+    0,                         /*tp_getattr*/
+    0,                         /*tp_setattr*/
+    0,                         /*tp_compare*/
+    0,                         /*tp_repr*/
+    0,                         /*tp_as_number*/
+    0,                         /*tp_as_sequence*/
+    0,                         /*tp_as_mapping*/
+    0,                         /*tp_hash */
+    0,                         /*tp_call*/
+    0,                         /*tp_str*/
+    0,                         /*tp_getattro*/
+    0,                         /*tp_setattro*/
+    0,                         /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_ITER,
+      /* tp_flags: Py_TPFLAGS_HAVE_ITER tells python to
+         use tp_iter and tp_iternext fields. */
+    "SnapshotIterator",                /* tp_doc */
+    0,                         /* tp_traverse */
+    0,                         /* tp_clear */
+    0,                         /* tp_richcompare */
+    0,                         /* tp_weaklistoffset */
+    (getiterfunc)SnapshotIterator_iter,  /* tp_iter: __iter__() method */
+    (iternextfunc)SnapshotIterator_iternext,  /* tp_iternext: next() method */
+	SnapshotIterator_Methods,          /* tp_methods */
+	0,                         /* tp_members */
+	0,                         /* tp_getset */
+	0,                         /* tp_base */
+	0,                         /* tp_dict */
+	0,                         /* tp_descr_get */
+	0,                         /* tp_descr_set */
+	0,                         /* tp_dictoffset */
+	(initproc)SnapshotIterator_init,   /* tp_init */
+	0                          /* tp_alloc */
+};
+
+static int SnapshotIterator_init(SnapshotIterator *self, PyObject *args) {
+	char *filename = NULL;
+	//size_t len = 0;
+	self->open_exception = 0;
+	
+	if (!PyArg_ParseTuple(args, "s", &filename)) {
+		return 1;
+	}
+	
+	tnt_snapshot(&(self->stream));
+
+	if (tnt_snapshot_open(&(self->stream), filename) == -1) {
+		self->open_exception = 1;
+		return 1;
+	}
+
+	
+	
+	//printf("arg: %s\n", data);
+	return 0;
+}
+
+PyObject* SnapshotIterator_iter(SnapshotIterator* self) {
+	if (self->open_exception) {
+		PyErr_Format(SnapshotError, "Can't open snapshot");
+		return NULL;
+	}
+	tnt_iter_storage(&(self->iter), &(self->stream));
+	Py_INCREF(self);
+	return (PyObject*)self;
+}
+
+PyObject* SnapshotIterator_iternext(SnapshotIterator* self) {
+	if (tnt_next(&(self->iter))) {
+		struct tnt_iter_storage *is = TNT_ISTORAGE(&(self->iter));
+	struct tnt_stream_snapshot* ss = TNT_SSNAPSHOT_CAST(TNT_ISTORAGE_STREAM(&(self->iter)));
+
+		PyObject *tuple = PyList_New(0);
+		PyObject *s;
+		
+		tnt_iter(&(self->iter_tuple), &is->t);
+		while (tnt_next(&(self->iter_tuple))) {
+			char *data = TNT_IFIELD_DATA(&(self->iter_tuple));
+			uint32_t size = TNT_IFIELD_SIZE(&(self->iter_tuple));
+			s = PyString_FromStringAndSize(data, size);
+			PyList_Append(tuple, s);
+			Py_DECREF(s);
+			//printf("size: %d\n", size);
+		}
+		if (self->iter_tuple.status == TNT_ITER_FAIL){
+			PyErr_Format(SnapshotError, "parsing error");
+			return NULL;
+		}
+		tnt_iter_free(&(self->iter_tuple));
+		
+		PyObject* tuple_as_tuple = PyList_AsTuple(tuple);
+		PyObject* ret = Py_BuildValue("(I,O)", ss->log.current.row_snap.space, tuple_as_tuple);
+		Py_DECREF(tuple_as_tuple);
+		Py_DECREF(tuple);
+		return ret;
+	}
+	if (self->iter.status == TNT_ITER_FAIL) {
+		PyErr_Format(SnapshotError, "Parsing failed: %s", tnt_snapshot_strerror(&(self->stream)));
+		return NULL;
+	}
+	return NULL;
+}
+
+PyObject* SnapshotIterator_del(SnapshotIterator* self) {
+	tnt_iter_free(&(self->iter));
+	tnt_stream_free(&(self->stream));
+	PyObject_Del(self);
+  
+	Py_RETURN_NONE;
+}
+
+static PyMethodDef TarantoolSnapshot_Module_Methods[] = {
+    {NULL}  /* Sentinel */
+};
+
+PyMODINIT_FUNC inittarantool_snapshot(void) {
+    PyObject *m;
+
+    SnapshotIterator_Type.tp_new = PyType_GenericNew;
+    if (PyType_Ready(&SnapshotIterator_Type) < 0)  return;
+
+    m = Py_InitModule("tarantool_snapshot", TarantoolSnapshot_Module_Methods);
+    if (!m) {
+        return;
+    }
+
+    Py_INCREF(&SnapshotIterator_Type);
+    PyModule_AddObject(m, "iter", (PyObject *)&SnapshotIterator_Type);
+
+    SnapshotError = PyErr_NewException("tarantool_snapshot.SnapshotError", NULL, NULL);
+    Py_INCREF(SnapshotError);
+    PyModule_AddObject(m, "SnapshotError", SnapshotError); 
+}
+
+int main(int argc, char **argv) {
+    Py_SetProgramName(argv[0]);
+    Py_Initialize();
+    inittarantool_snapshot();
+    return 0;
+}
